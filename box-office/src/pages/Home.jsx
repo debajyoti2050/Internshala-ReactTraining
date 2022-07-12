bc06595cb9d58a03b015395a70dc422aab183189
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-07-12 12:14:20 +0530

    display api results

--- box-office/src/pages/Home.jsx
+++ box-office/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input,setInput]= useState('')
   const[results, setResults]= useState(null)
 
   const onSearch= ()=>{
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-    .then(response => response.json())
-    .then(result =>{
-      setResults(result)
-      console.log(result)
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
     })
   }
 
@@ -24,6 +22,22 @@ const Home = () => {
     }
   }
 
+  const renderResults =()=>{
+    if (results && results.length ===0){
+      return <div>No Results</div>
+
+    }
+    if(results && results.length > 0){
+      return(<div>{
+        results.map((item) =>(<div key={item.show.id}>{item.show.name}</div>))
+        }</div>)
+
+    }
+
+    return null;
+
+
+  }
   
 
 
@@ -32,7 +46,7 @@ const Home = () => {
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}></input>
       <button type='button' onClick={onSearch}>Search</button>
-      {renderresults()}
+      {renderResults()}
     </MainPageLayout>
   )
 }
